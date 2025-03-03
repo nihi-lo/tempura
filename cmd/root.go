af384c2988b2913b3863c77a13459fc4eb030c8a
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2025-03-04 01:04:04 +0900

    🔖 v0.2.0リリース

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Version: "0.1.0",
+	Version: "0.2.0",
 	Use:     "tempura",
 	Short:   `A CLI tool that leverages templates to smoothly create projects.`,
 	Long: `  ______________  _______  __  ____    ___ 
