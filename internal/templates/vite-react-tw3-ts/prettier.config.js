2baf28a8331ef9d753af5f93f26f8189a630a8cf
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2025-04-24 00:54:04 +0900

    🔥 vite-react-tw3-ts: テンプレートを削除

--- internal/templates/vite-react-tw3-ts/prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  endOfLine: "auto",
-  printWidth: 100,
-};
-
-export default config;
--- internal/templates/vite-react-tw3-ts/src/components/HelloWorldText/HelloWorldText.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const HelloWorldText: React.FC = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
